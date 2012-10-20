f5aadd72d64861997b580934775a7692056a813a
Loc Nguyen <lochnguyen@gmail.com>
2012-10-19 23:13:57 -0700

    adding color to spec result output

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,5 +2,7 @@ require 'data_mapper'
 require 'dm-rspec2'
 
 RSpec.configure do |config|
-  config.include(DataMapper::Matchers)
+  config.include DataMapper::Matchers
+  config.color_enabled = true
+  config.tty = true
 end
