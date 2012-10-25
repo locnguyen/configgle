acefc3f370cc7154eac4b40ea2d65d4df33e2b19
Loc Nguyen <lochnguyen@gmail.com>
2012-10-25 15:14:31 -0700

    Changed the code example to be more explicit in what it expects

--- lib/registration.rb
+++ lib/registration.rb
@@ -9,9 +9,12 @@ class Registration
   belongs_to :event
   belongs_to :member
 
+  attr_accessor :event, :member, :completed_date
+
   def initialize(attributes = {})
     raise(ArgumentError, 'Need an event to create a registration', caller) if attributes[:event].nil?
     raise(ArgumentError, 'Need member to create a registration', caller) if attributes[:member].nil?
 
+    attributes.each { |k, v| instance_variable_set("@#{k}", v) unless v.nil? }
   end
 end
--- spec/registration_spec.rb
+++ spec/registration_spec.rb
@@ -12,10 +12,22 @@ describe Registration do
   it { should belong_to :member }
 
   it "cannot not be created without an event" do
-    expect { Registration.new( member: double('member') )}.to raise_error
+    expect {
+      Registration.new( member: double('member') )
+    }.to raise_error(ArgumentError, 'Need an event to create a registration')
   end
 
-  it "cannot be created without an member" do
-    expect { Registration.new( event: double('event') )}.to raise_error
+  it "cannot be created without a member" do
+    expect {
+      Registration.new( event: double('event'), member: nil )
+    }.to raise_error(ArgumentError, 'Need member to create a registration')
+  end
+
+  it "is created with an event" do
+    subject.event.should be
+  end
+
+  it "is created with a member" do
+    subject.member.should be
   end
 end
